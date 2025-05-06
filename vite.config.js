c8e608b9352371ad57de4b85989d4b6366f14b5c
Martinonyte Dovile <71691245+dovile-mart@users.noreply.github.com>
2025-05-06 11:11:36 +0300

    Deployment changes added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/dovile-mart/react-test.git',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
