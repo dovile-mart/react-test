8e298e97d83e5cc2028772c0460a09001ce4546a
Martinonyte Dovile <71691245+dovile-mart@users.noreply.github.com>
2025-05-06 11:56:10 +0300

    cors changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/dovile-mart/react-test/',
+  base: '/react-test/',
   plugins: [react()],
   test: {
     globals: true,
