d7ad55f367d9ce738293502f4b2c52523258e863
Martinonyte Dovile <71691245+dovile-mart@users.noreply.github.com>
2025-05-06 11:19:45 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/dovile-mart/react-test.git',
+  base: 'https://github.com/dovile-mart/react-test',
   plugins: [react()],
   test: {
     globals: true,
